634379f3ce595a12a8534079c2fb669e2b251074
shreya_baldha <90861621+baldhashreya@users.noreply.github.com>
2026-01-07 16:30:43 +0530

    complete changes

--- to-do-list/src/App.tsx
+++ to-do-list/src/App.tsx
@@ -1,15 +1,10 @@
-// import { useState } from "react";
 import "./App.css";
 import { List } from "./component/List";
-import { Plus } from "lucide-react";
 
 function App() {
   return (
     <>
       <div className="container">
-        <div className="add-task">
-          <Plus />
-        </div>
         <List />
       </div>
     </>
